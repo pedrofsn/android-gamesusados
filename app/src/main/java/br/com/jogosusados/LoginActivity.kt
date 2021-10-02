9ac5d57cbbe591b4e02b5364db073fc168487a06
pedrofsn <pedrokra@gmail.com>
2021-10-02 16:16:07 -0300

    Preparing login feature

--- app/src/main/java/br/com/jogosusados/LoginActivity.kt
+++ app/src/main/java/br/com/jogosusados/LoginActivity.kt
@@ -1,7 +1,9 @@
 package br.com.jogosusados
 
+import android.view.View
 import br.com.jogosusados.databinding.ActivityLoginBinding
 import br.com.redcode.base.mvvm.restful.databinding.domain.ActivityMVVM
+import br.com.redcode.easyvalidation.Validate
 
 class LoginActivity : ActivityMVVM<ActivityLoginBinding, LoginViewModel>() {
 
@@ -9,7 +11,17 @@ class LoginActivity : ActivityMVVM<ActivityLoginBinding, LoginViewModel>() {
     override val layout = R.layout.activity_login
 
     override fun afterOnCreate() {
-        viewModel.load()
+
+    }
+
+    fun validateForm(view: View?) {
+        if (Validate.isFilled(
+                binding.textInputEditTextUsername,
+                binding.textInputEditTextPassword
+            )
+        ) {
+            viewModel.login()
+        }
     }
 
 }
--- app/src/main/java/br/com/jogosusados/LoginViewModel.kt
+++ app/src/main/java/br/com/jogosusados/LoginViewModel.kt
@@ -1,11 +1,17 @@
 package br.com.jogosusados
 
+import androidx.databinding.ObservableField
 import br.com.redcode.easyrestful.library.impl.viewmodel.BaseViewModel
 
 class LoginViewModel : BaseViewModel() {
 
-    fun load() {
+    val username = ObservableField<String>()
+    val password = ObservableField<String>()
 
+    fun validateForm() = sendEventToUI("validateForm")
+
+    fun login() {
+        showMessage(username.get() + " => " + password.get())
     }
 
 }
