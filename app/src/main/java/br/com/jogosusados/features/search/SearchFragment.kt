d37efb19248c8fdc35bc669f7911e6e7b59eedff
pedrofsn <pedrokra@gmail.com>
2021-11-08 22:32:18 -0300

    Creating search screen

--- app/src/main/java/br/com/jogosusados/features/search/SearchFragment.kt
+++ app/src/main/java/br/com/jogosusados/features/search/SearchFragment.kt
@@ -1,16 +1,43 @@
 package br.com.jogosusados.features.search
 
+import androidx.recyclerview.widget.GridLayoutManager
 import br.com.jogosusados.R
 import br.com.jogosusados.databinding.FragmentSearchBinding
+import br.com.jogosusados.features.games.AdapterGameItem
+import br.com.jogosusados.features.games.LabelGames
+import br.com.redcode.base.mvvm.extensions.observer
 import br.com.redcode.base.mvvm.restful.databinding.impl.FragmentMVVMDataBinding
+import br.com.redcode.easyrecyclerview.library.extension_functions.setCustomAdapter
 
 class SearchFragment : FragmentMVVMDataBinding<FragmentSearchBinding, SearchViewModel>() {
 
     override val classViewModel = SearchViewModel::class.java
     override val layout = R.layout.fragment_search
 
+    private val observer = observer<LabelGames> { updateUI(it) }
+
+    private val adapter = AdapterGameItem { item, position ->
+
+    }
+
+    override fun setupUI() {
+        super.setupUI()
+        viewModel.liveData.observe(this, observer)
+    }
+
     override fun afterOnCreate() {
+        val layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.recyclerView.setCustomAdapter(adapter, layoutManager)
+    }
+
+    override fun onResume() {
+        super.onResume()
         viewModel.load()
+    }
+
+    private fun updateUI(data: LabelGames) {
+        adapter.setCustomList(data.games)
+        hideProgress()
     }
 
 }
--- app/src/main/java/br/com/jogosusados/features/search/SearchViewModel.kt
+++ app/src/main/java/br/com/jogosusados/features/search/SearchViewModel.kt
@@ -1,11 +1,22 @@
 package br.com.jogosusados.features.search
 
+import br.com.jogosusados.features.games.GameItem
+import br.com.jogosusados.features.games.LabelGames
 import br.com.redcode.easyrestful.library.impl.viewmodel.BaseViewModelWithLiveData
 
-class SearchViewModel : BaseViewModelWithLiveData<LabelSearch>() {
+class SearchViewModel : BaseViewModelWithLiveData<LabelGames>() {
 
     override fun load() {
-
+        val games = arrayListOf<GameItem>()
+        for (i in 1..10) {
+            games.add(GameItem(
+                id = i.toLong(),
+                image = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSIee3Eac7z5dBDmR6d_pCvnDdU9xhjpv9oTTW7ladJKGD3xTJjYpFK1i6x84I8dJ4uxir2YdTBYwkAgJP3-9c6fi6dGeD6O7Oy-835i72HaeMXMIBEG9Ks8w&usqp=CAE",
+                title = "Ghost of Tsushima",
+                subtitle = "PS4"
+            ))
+        }
+        liveData.postValue(LabelGames(games))
     }
 
 }
