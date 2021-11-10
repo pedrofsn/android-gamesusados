0b2afed9d439625e8a8695e0556d7342403c4489
pedrofsn <pedrokra@gmail.com>
2021-11-09 23:29:51 -0300

    Starting register screen

--- app/src/main/java/br/com/jogosusados/features/add/AddFragment.kt
+++ app/src/main/java/br/com/jogosusados/features/add/AddFragment.kt
@@ -3,6 +3,8 @@ package br.com.jogosusados.features.add
 import br.com.jogosusados.R
 import br.com.jogosusados.databinding.FragmentAddBinding
 import br.com.redcode.base.mvvm.restful.databinding.impl.FragmentMVVMDataBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 
 class AddFragment : FragmentMVVMDataBinding<FragmentAddBinding, AddViewModel>() {
 
@@ -10,7 +12,24 @@ class AddFragment : FragmentMVVMDataBinding<FragmentAddBinding, AddViewModel>() 
     override val layout = R.layout.fragment_add
 
     override fun afterOnCreate() {
+        addChip("Playstation 5")
+        addChip("Xbox One")
+        addChip("Switch")
+        addChip("3DS")
+        addChip("PSP")
+        addChip("N64")
+        binding.chipGroup.setOnCheckedChangeListener { _, checkedId -> toast(checkedId.toString()) }
+    }
 
+    private fun addChip(text: String) {
+        val context = requireContext()
+        val styleChoice = R.style.Widget_MaterialComponents_Chip_Choice
+        Chip(context).apply {
+            val chipDrawable = ChipDrawable.createFromAttributes(context, null, 0, styleChoice);
+            setChipDrawable(chipDrawable)
+            this.text = text
+            binding.chipGroup.addView(this)
+        }
     }
 
 }
