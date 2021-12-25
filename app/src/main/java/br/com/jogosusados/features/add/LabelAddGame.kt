f5c47ca8cea3ca330c20adeff82034b6ecb2d879
pedrofsn <pedrokra@gmail.com>
2021-12-25 12:49:06 -0300

    Fixing data type

--- app/src/main/java/br/com/jogosusados/features/add/LabelAddGame.kt
+++ app/src/main/java/br/com/jogosusados/features/add/LabelAddGame.kt
@@ -1,9 +1,10 @@
 package br.com.jogosusados.features.add
 
+import br.com.jogosusados.features.add.data.IdWithTitle
 import br.com.jogosusados.features.games.GameItem
 
 data class LabelAddGame(
-    val platforms: List<String>,
-    val game: GameItem?,
-    val value: String
+    val platforms: List<IdWithTitle>,
+    val game: GameItem? = null,
+    val value: String = ""
 )
