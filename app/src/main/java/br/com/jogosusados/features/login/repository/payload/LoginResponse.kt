a04c6b09fe0676e88cc52bd0c46b9a655dbaf650
pedrofsn <pedrokra@gmail.com>
2021-12-25 12:46:56 -0300

    Fixing response code

--- app/src/main/java/br/com/jogosusados/features/login/repository/payload/LoginResponse.kt
+++ app/src/main/java/br/com/jogosusados/features/login/repository/payload/LoginResponse.kt
@@ -5,5 +5,5 @@ import br.com.redcode.base.interfaces.Payload
 private const val TYPE = "Bearer"
 
 data class LoginResponse(val token: String, val type: String = TYPE) : Payload<LoggedDTO> {
-    override fun toModel() = LoggedDTO("", "")
+    override fun toModel() = LoggedDTO(token, type)
 }
