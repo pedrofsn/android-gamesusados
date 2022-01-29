84eae51045d8d4015f66eca9cde44a06ca54c217
pedrofsn <pedrokra@gmail.com>
2022-01-29 12:35:22 -0300

    Adding support to kotlin parcelize

--- app/src/main/java/br/com/jogosusados/features/games/list/GameItem.kt
+++ app/src/main/java/br/com/jogosusados/features/games/list/GameItem.kt
@@ -1,8 +1,12 @@
 package br.com.jogosusados.features.games.list
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameItem(
     val id: Long,
     val image: String,
     val title: String,
     val subtitle: String
-)
+) : Parcelable
