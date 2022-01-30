4f67359c6815d12e355454f0a615f61f6394fe48
pedrofsn <pedrokra@gmail.com>
2022-01-29 22:35:00 -0300

    Adding support to save game announcement in repository layer

--- app/src/main/java/br/com/jogosusados/features/add/repository/AddRepository.kt
+++ app/src/main/java/br/com/jogosusados/features/add/repository/AddRepository.kt
@@ -1,8 +1,10 @@
 package br.com.jogosusados.features.add.repository
 
+import br.com.jogosusados.features.add.data.GameAnnouncement
 import br.com.jogosusados.features.add.data.IdWithTitle
 import br.com.jogosusados.network.Requestable
 
 interface AddRepository : Requestable {
     suspend fun getPlatforms(): List<IdWithTitle>
+    suspend fun saveAnnouncement(idGame: Long, value: String): GameAnnouncement?
 }
--- app/src/main/java/br/com/jogosusados/features/add/repository/AddRepositoryImpl.kt
+++ app/src/main/java/br/com/jogosusados/features/add/repository/AddRepositoryImpl.kt
@@ -2,6 +2,7 @@ package br.com.jogosusados.features.add.repository
 
 import br.com.jogosusados.features.add.data.IdWithTitle
 import br.com.jogosusados.network.Request
+import br.com.jogosusados.network.Request.call
 import br.com.jogosusados.network.Request.callList
 import br.com.jogosusados.network.Request.handled
 import br.com.redcode.easyreftrofit.library.CallbackNetworkRequest
@@ -13,4 +14,8 @@ class AddRepositoryImpl(
     override suspend fun getPlatforms(): List<IdWithTitle> {
         return (Request with api callList { getPlatforms() } handled callbackNetworkRequest).orEmpty()
     }
+
+    override suspend fun saveAnnouncement(idGame: Long, value: String) = Request with api call {
+        saveAnnouncement(idGame, value)
+    } handled callbackNetworkRequest
 }
