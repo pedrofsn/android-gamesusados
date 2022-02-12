4aef06454865ac6d7917b0d8ea60696e46fc28f7
pedrofsn <pedrokra@gmail.com>
2022-02-12 19:00:39 -0300

    Deleting unused class

--- app/src/main/java/br/com/jogosusados/features/search/LabelSearch.kt
+++
@@ -1,9 +0,0 @@
-package br.com.jogosusados.features.search
-
-data class LabelSearch(
-    val id: Long,
-    val value: String,
-    val valueA: String,
-    val valueB: String,
-    val valueC: String,
-)
