a520692a601e2c2c8be12aad777fc0af5771e4a4
pedrofsn <pedrokra@gmail.com>
2022-08-09 22:35:31 -0300

    Adding to report repository koin dependency

--- app/src/main/java/br/com/jogosusados/features/announcement/di/GameAnnouncementModules.kt
+++ app/src/main/java/br/com/jogosusados/features/announcement/di/GameAnnouncementModules.kt
@@ -4,6 +4,9 @@ import br.com.jogosusados.features.announcement.GameAnnouncementViewModel
 import br.com.jogosusados.features.announcement.repository.GameAnnouncementRepository
 import br.com.jogosusados.features.announcement.repository.GameAnnouncementRepositoryImpl
 import br.com.jogosusados.features.announcement.repository.GameAnnouncementsAPI
+import br.com.jogosusados.features.announcement.repository.ToReportAPI
+import br.com.jogosusados.features.announcement.repository.ToReportRepository
+import br.com.jogosusados.features.announcement.repository.ToReportRepositoryImpl
 import br.com.jogosusados.network.NetworkModule
 import br.com.redcode.easyreftrofit.library.CallbackNetworkRequest
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -18,6 +21,15 @@ object GameAnnouncementModules {
 
         factory<GameAnnouncementRepository> { (callback: CallbackNetworkRequest?) ->
             GameAnnouncementRepositoryImpl(
+                api = get(),
+                callbackNetworkRequest = callback
+            )
+        }
+
+        single { get<Retrofit>(NetworkModule.NetworkRegular).create(ToReportAPI::class.java) }
+
+        factory<ToReportRepository> { (callback: CallbackNetworkRequest?) ->
+            ToReportRepositoryImpl(
                 api = get(),
                 callbackNetworkRequest = callback
             )
