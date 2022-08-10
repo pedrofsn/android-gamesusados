d1a2194b88348cd279c1739b1ac64a91e7c1e8cd
pedrofsn <pedrokra@gmail.com>
2022-08-09 22:33:22 -0300

    Specify return type explicity

--- app/src/main/java/br/com/jogosusados/features/announcement/data/Announcement.kt
+++ app/src/main/java/br/com/jogosusados/features/announcement/data/Announcement.kt
@@ -3,14 +3,15 @@ package br.com.jogosusados.features.announcement.data
 import android.os.Parcelable
 import br.com.concrete.canarinho.formatador.FormatadorValor.VALOR_COM_SIMBOLO
 import br.com.jogosusados.features.add.data.Owner
+import br.com.jogosusados.features.search.domain.WithID
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Announcement(
-    val id: Long,
+    override val id: Long,
     val owner: Owner,
     val price: Double,
-): Parcelable {
-    fun getLabelPrice() = VALOR_COM_SIMBOLO.formata(price.toString())
+) : Parcelable, WithID {
+    fun getLabelPrice(): String = VALOR_COM_SIMBOLO.formata(price.toString())
 }
 
