3917297064e0ae10fdb1bfde289bd6b2f4f5d5cb
pedrofsn <pedrokra@gmail.com>
2022-08-09 22:35:14 -0300

    Creating get id function

--- app/src/main/java/br/com/jogosusados/features/toreport/ToReport.kt
+++ app/src/main/java/br/com/jogosusados/features/toreport/ToReport.kt
@@ -1,17 +1,26 @@
 package br.com.jogosusados.features.toreport
 
+import android.os.Parcelable
 import br.com.jogosusados.features.announcement.data.Announcement
 import br.com.jogosusados.features.search.data.GameItem
 import br.com.jogosusados.features.toreport.ToReportType.ANNOUNCEMENT
 import br.com.jogosusados.features.toreport.ToReportType.GAME
+import br.com.redcode.base.utils.Constants.INVALID_VALUE
+import kotlinx.android.parcel.Parcelize
 
-data class ToReport(val game: GameItem?, val announcement: Announcement?) {
+@Parcelize
+data class ToReport(val game: GameItem?, val announcement: Announcement?) : Parcelable {
     val type: ToReportType
         get() = when {
             game != null -> GAME
             announcement != null -> ANNOUNCEMENT
             else -> throw RuntimeException("Tipo não suportado")
         }
+
+    fun getId() = when (type) {
+        GAME -> game?.id
+        ANNOUNCEMENT -> announcement?.id
+    } ?: INVALID_VALUE
 
     fun getTitle() = when (type) {
         GAME -> game?.title
